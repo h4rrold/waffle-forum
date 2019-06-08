6b7e98af0b5dc5a2ad71316b256f6386281d60fb
Dmitriy Nickolay Petruk <mistake952@mail.ru>
2019-06-08 16:58:31 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,10 @@ ob_start();
 session_start();
 //$_SESSION["loggedin"] = false;
 require_once 'Core/config.php';
-MyPDO::connect("mysql:host=$server;dbname=$db;charset=utf8", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
-
-
 require_once 'Core/Middleware.php';
 require_once 'Core/MyPDO.php';
+MyPDO::connect("mysql:host=$server;dbname=$db;charset=utf8", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
+
 require_once 'Core/Templator.php';
 require_once 'Core/Route.php';
 require_once 'App/Route/web.php';
