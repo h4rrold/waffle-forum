bdac5270b891782ccf549d0d4f20b519830666f1
vfrmn <49616737+vfrmn@users.noreply.github.com>
2019-06-09 18:22:29 +0300

    discussion

--- App/Controllers/CategoriesController.php
+++ App/Controllers/CategoriesController.php
@@ -1,17 +1,25 @@
 <?php
 require_once "App/Category.php";
+require_once "App/Topic.php";
 class CategoriesController extends  Controller
 {
-    public function out($categories)
+    public function out($params = [])
     {
-        echo output('categories', ['categories' => $categories]);
+        echo output('categories', $params );
     }
 
     public function getCategories()
     {
         $obj = new Category();
         $categories = $obj->getFetchCategories();
-        $this->out($categories);
+        $this->out(['categories' => $categories]);
+    }
+
+    public function getTopics($id_category, $category)
+    {
+        $obj = new Topic();
+        $topics = $obj->getFetchTopics($id_category);
+        $this->out(['topics' => $topics, 'id_category' => $id_category, 'category'=> $category]);
     }
 
 }
---
+++ App/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitaliy
+ * Date: 09.06.2019
+ * Time: 17:30
+ */
+
+class User extends Model
+{
+
+}
