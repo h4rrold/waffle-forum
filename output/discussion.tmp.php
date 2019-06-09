af99cf84d27ddf99c68369626c42c9550c5bd3bd
h4rrold <voronkov.g4@gmail.com>
2019-06-09 17:29:03 +0300

    Create discussion.tmp.php

---
+++ output/discussion.tmp.php
@@ -0,0 +1,99 @@
+<div class="content">
+        <div class="content__page">
+            <div class="page">
+                <div class="page__discussion-path">
+                    <span class="page__discussion-path__text">Категорії<i class="fas fa-chevron-right"></i>Розробка</span>
+                </div>
+                <div class="pagе__discussion">
+                    <div class="discussion">
+                        <div class="discussion__topic">
+                            <div id="idTopic" class="topic">
+                                <div class="topic__user-avatar">
+                                    <a href="#" class="user-avatar">
+                                         <img src="img/user-avatar.png" alt="">
+                                    </a>
+                                </div>
+                                <div class="topic__topic-content">
+                                    <div class="topic-content__title-topic">
+                                        <div class="title-topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                                    </div>
+                                    <div class="topic-content__info-stat">
+                                        <div class="info-stat">
+                                            <div class="info-stat__fl-stat">
+                                                <div class="fl-stat">
+                                                    <span class="fl-stat__topic-author">
+                                                        <a href="#" class="topic-author">Tommy</a>
+                                                    </span>
+                                                    <span class="fl-stat__delimeter">|</span>
+                                                    <span class="fl-stat__group-author">Користувач</span>
+                                                    <span class="fl-stat__delimeter">|</span>
+                                                    <span class="fl-stat__topic-post-date">12 травня 2019</span>
+                                                </div>
+                                            </div>
+                                            <div class="info-stat__fr-stat">
+                                                <div class="fr-stat">
+                                                    <span class="fr-stat__post-amount">256<i class="far fa-comment-dots"></i></span>
+                                                    <span class="stat__delimeter">|</span>
+                                                    <span class="fr-stat__views-amount">1231<i class="far fa-eye"></i></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="topic-content__text-topic">
+                                        <p class="text-topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco...is nostrud exercitation ullamco...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco...is nostrud exercitation ullamco...</p>
+                                    </div>              
+                                </div>
+                            </div>
+                        </div>
+                        <div class="discussion__post">
+                            <div class="post">
+                                <div class="post__user-avatar">
+                                    <a href="" class="user-avatar">
+                                        <img src="img/user-avatar.png">
+                                    </a>
+                                </div>
+                                <div class="post__post-content">
+                                    <div class="post-content__info-stat-post">
+                                        <div class="info-stat-post">
+                                            <span class="info-stat-post__post-author">
+                                                    <a href="#" class="post-author">Jacob</a>
+                                            </span>
+                                            <span class="stat__delimeter">|</span>
+                                            <span class="info-stat-post__group-author">Користувач</span>
+                                            <span class="stat__delimeter">|</span>
+                                            <span class="info-stat-post__post-date">12 травня 2019</span>
+                                        </div>
+                                        <div class="post-content_post-text">
+                                            <p class="post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                                    ullamco... </p>
+                                        </div>
+                                        <div class="post-content__rate-post">
+                                            <div class="rate-post">
+                                                <span class="rate-post__text">Чи був цей коментар корисний?</span>
+                                                <button class="rate-post__button">Так</button>
+                                                <button class="rate-post__button">Ні</button>
+                                            </div>
+                                            <div class="rate-post_result">
+                                                <span class="rate-post__text_result">Дякуємо за ваш відгук!</span>
+                                                <div class="rate-post__stat-post_result">
+                                                    <i class="fas fa-thumbs-up"></i>
+                                                    <span class="stat-post__positive">5</span>
+                                                    <span class="stat__delimeter">|</span>
+                                                    <i class="fas fa-thumbs-down"></i>
+                                                    <span class="stat-post__negative">5</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                                 
+                    </div>              
+                </div>
+                <div class="page__sign-in-proposition-button">
+                    <button class="sign-in-proposition-button">Увійти, щоб залишити коментар</button>
+                </div>
+            </div>              
+        </div>
+    </div>
