71d2f61b0972fab85cb3331e2f9cd77258dbb552
zigutt <byblukmistake@gmail.com>
2019-06-10 19:07:18 +0300

    Added ROUTE_PATH

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+if(dirname($_SERVER['SCRIPT_NAME']) == '/')
+{
+    define(ROUTE_PATH, '');
+}
+else define(ROUTE_PATH, $_SERVER['SCRIPT_NAME']);
+
 ob_start();
 session_start();
 require_once 'Core/config.php';
@@ -11,7 +17,6 @@ require_once 'Core/Controller.php';
 require_once 'Core/Model.php';
 require_once 'Core/SQLBuilder.php';
 require_once 'Core/App.php';
-echo dirname($_SERVER['SCRIPT_NAME']);
 MyPDO::connect("mysql:host=$server;dbname=$db;charset=utf8", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
 echo output('header');
 Route::run();
