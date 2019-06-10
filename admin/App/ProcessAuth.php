c28900f9abe6c4e846128059b5249e927f9d22b0
zigutt <byblukmistake@gmail.com>
2019-06-10 20:10:10 +0300

    Added permission to admin panel

--- admin/App/ProcessAuth.php
+++ admin/App/ProcessAuth.php
@@ -4,13 +4,16 @@ class ProcessAuth extends Model
 {
     public function validateCredentials($username, $password, $username_err, $password_err)
     {
-        if(empty($username_err) && empty($password_err)){
-            
+        if(empty($username_err) && empty($password_err))
+        {   
             $sql = "SELECT id, nickname, pass, group_id FROM users WHERE nickname = ?";
             $result = MyPDO::run($sql, [$username]);
-            if(count($result) > 0){                    
+            if(count($result) > 0)
+            {                    
                 $hashed_password = $result[0]['pass'];
-                    if(password_verify($password, $hashed_password))
+                if(password_verify($password, $hashed_password))
+                {
+                    if($result[0]['group_id'] == 1)
                     {
                         session_start();
                         $_SESSION["loggedin"] = true;
@@ -18,12 +21,15 @@ class ProcessAuth extends Model
                         $_SESSION["username"] = $username;                            
                         
                         header("location: panel");
-                    } else{
-                        $password_err = "The password you entered was not valid.";
+                    } else {
+                        $username_err = "You don't have permissions to that site."
                     }
+                } else {
+                    $password_err = "The password you entered was not valid.";
                 }
-            } else{
+            } else {
                 $username_err = "No account found with that username.";
+            } 
         }
         return [$username_err, $password_err];
     }
