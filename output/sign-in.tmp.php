3ee3dfc403ce390ce2649dbd309abf2a423f0a9e
h4rrold <voronkov.g4@gmail.com>
2019-06-10 17:38:49 +0300

    add sign-in page

---
+++ output/sign-in.tmp.php
@@ -0,0 +1,33 @@
+<div class="content_no-margin">
+        <div class="content__page-sign-in">
+            <div class="page-sign-in">
+                <div class="page-sign-in__page-title">
+                    <h1 class="page-title_white">Реєстрація</h1>
+                    <h2 class="page-subtitle_white">створи свій акаунт в спільноті Waffle вже зараз!</h2>
+                </div>
+                <div class="page-sign-in__container-sign-in">
+                    <div class="container-sign-in">
+                        <div class="container-sign-in__sign-in-form">
+                            <form action="?''" class="sign-in-form" type="post">
+                                    <div class="sign-in-form__input-item">
+                                            <div class="input-item__text">Email</div>
+                                            <input type="email" name="" id="" class="input-item">
+                                        <!--навешен display для first-child--><div class="input-item__text-error">Ви ввели некоректний email</div>
+                                        </div>
+                                <div class="sign-in-form__input-item">
+                                   <div class="input-item__text">Пароль</div>
+                                    <input type="text" name="" id="" class="input-item">
+                                    <div class="input-item__text-error">Ви ввели невірний пароль</div>
+                                </div>
+                    
+                                <div class="sign-in-form__button-sign-in">
+                                    <input type="submit" value="Увійти" class="button-sign-in">
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
