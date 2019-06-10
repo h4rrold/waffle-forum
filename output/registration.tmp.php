ecebe1416c8a1603cd3a468439b5327c08d3b03f
h4rrold <voronkov.g4@gmail.com>
2019-06-10 05:50:29 +0300

    add registration

---
+++ output/registration.tmp.php
@@ -0,0 +1,42 @@
+ <div class="content_no-margin">
+        <div class="content__page-registration">
+            <div class="page-registration">
+                <div class="page-registration__page-title">
+                    <h1 class="page-title_white">Реєстрація</h1>
+                    <h2 class="page-subtitle_white">створи свій акаунт в спільноті Waffle вже зараз!</h2>
+                </div>
+                <div class="page-registration__container-registration">
+                    <div class="container-registration">
+                        <div class="container-registration__reg-form">
+                            <form action="?''" class="reg-form" type="post">
+                                    <div class="reg-form__input-item">
+                                            <div class="input-item__text">Email</div>
+                                            <input type="email" name="" id="" class="input-item">
+                                        <!--навешен display для first-child--><div class="input-item__text-error">Ви ввели некоректний email</div>
+                                        </div>
+                                <div class="reg-form__input-item">
+                                   <div class="input-item__text">Нік</div>
+                                    <input type="text" name="" id="" class="input-item">
+                                    <div class="input-item__text-error">Текст помилки буде тут</div>
+                                </div>
+                                <div class="reg-form__input-item">
+                                    <div class="input-item__text">Пароль</div>
+                                    <input type="text" name="" id="" class="input-item">
+                                    <div class="input-item__text-error">Текст помилки буде тут</div>
+                                </div>
+                                <div class="reg-form__input-item">
+                                    <div class="input-item__text">Повторіть пароль</div>
+                                    <input type="text" name="" id="" class="input-item"> 
+                                    <div class="input-item__text-error">Текст помилки буде тут</div>                                           
+                                </div>
+                                <div class="reg-form__button-create">
+                                    <input type="submit" value="Створити акаунт" class="button-create">
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
