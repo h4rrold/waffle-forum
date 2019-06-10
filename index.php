aed6ba58b4b8fb2d835efd00581557e8bb29347a
zigutt <byblukmistake@gmail.com>
2019-06-10 17:54:05 +0300

    Preparing for production
    Added config.php for heroku

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+require_once 'Core/config.php';
 require_once "Core/Middleware.php";
 require_once 'Core/MyPDO.php';
 require_once 'Core/Templator.php';
@@ -10,7 +11,7 @@ require_once 'Core/Controller.php';
 require_once 'Core/Model.php';
 require_once 'Core/SQLBuilder.php';
 require_once 'Core/App.php';
-MyPDO::connect('mysql:host=localhost;dbname=waffle_forum;charset=utf8', 'root', 'admin', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
+MyPDO::connect("mysql:host=$server;dbname=$db;charset=utf8", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
 echo output('header');
 Route::run();
 echo output('footer');
